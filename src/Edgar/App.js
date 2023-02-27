a435660815408e019ef724ddb2089412c5c7e57e
BrownEdgar <nikogosjanedgar@gmail.com>
2023-02-27 14:23:24 +0400

    added delete example with props

--- src/Edgar/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function App() {
-	return (
-		<div>
-			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id amet nemo perspiciatis eius nesciunt, repellat excepturi? Nisi autem beatae, error eligendi tenetur perferendis at impedit expedita deserunt debitis perspiciatis iusto?</p>
-		</div>
-	)
-}
