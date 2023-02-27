0595b63d418000726e9f744c99e7f062f0750aef
GurgenBayramyan <gugobayramyan@gmail.com>
2023-02-27 16:20:47 +0400

    Component create Commit

---
+++ src/Gurgen/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function App(){
+    return(
+        <div >
+            <h1>this is my page</h1>
+        </div>
+    )
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Edgar/App';
+import App from './Gurgen/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
