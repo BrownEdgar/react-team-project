becf50546fc8ebc627cf2c3d791c7e20dceb2593
Hrayr777 <harutyunyan.12@inbox.ru>
2023-03-06 12:56:25 +0400

    s

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Gurgen/Homework 2 useEfect/App'
+import App from './Hrayr/workWithimages/App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
